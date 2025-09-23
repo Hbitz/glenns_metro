01014628b29d19d1da8b3c5282bf942e98ee2047
Hbitz <53080862+Hbitz@users.noreply.github.com>
2025-09-23 08:09:33 +0200

    Clean up build folder

--- meteo_app/build/libs/Cities.d
+++
@@ -1,5 +0,0 @@
-build/libs/Cities.o: src/libs/Cities.c src/libs/Cities.h src/libs/Meteo.h \
- src/libs/utils.h
-src/libs/Cities.h:
-src/libs/Meteo.h:
-src/libs/utils.h:
--- meteo_app/build/src/main.d
+++
@@ -1,5 +0,0 @@
-build/src/main.o: src/main.c src/libs/Cities.h src/libs/Meteo.h \
- src/libs/user.h
-src/libs/Cities.h:
-src/libs/Meteo.h:
-src/libs/user.h:
